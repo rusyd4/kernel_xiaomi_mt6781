7915d6d8db5f7373d0ca171393f5d37e30c968aa
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2019-06-10 12:25:23 +0530

    sched/fair: Fix excessive packing on the max capacity CPU
    When boost is enabled or mid capacity cluster has single cpu, then
    we traverse both mid and max capacity CPUs. When all of these CPUs
    are busy, the task should be placed on the CPU which has highest
    spare capacity. However the current code completely ignores
    the mid capacity CPU when it encounters the max capacity CPU,
    though the former has more spare capacity.
    
    Fix this issue by enforcing the spare capacity check across
    different capacity CPUs.
    
    Change-Id: I726d47f985f9e59d2bb1c6cf2b743796b57e3051
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    [render: minor fixup]
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7213,8 +7213,7 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 			 */
 
 			/* Favor CPUs with maximum spare capacity */
-			if (capacity_orig == target_capacity &&
-			    spare_cap < target_max_spare_cap)
+			if (spare_cap < target_max_spare_cap)
 				continue;
 
 			target_max_spare_cap = spare_cap;
