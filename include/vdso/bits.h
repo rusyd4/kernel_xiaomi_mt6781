80177617577f3c078ef5268e1de1fbb94b99bfa3
claxten10 <claxten10@gmail.com>
2024-11-10 16:13:59 +0100

    include: vdso: Resolve macro redefinitions of BIT
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- include/vdso/bits.h
+++ include/vdso/bits.h
@@ -7,11 +7,12 @@
  * Old version of this macro to preserve the CRC signatures of some drm symbols.
  * Crazy but true...
  */
-#define BIT(nr)			(1UL << (nr))
 #else
 #include <vdso/const.h>
 
+#ifndef BIT
 #define BIT(nr)			(UL(1) << (nr))
+#endif
 #endif
 
 #endif	/* __VDSO_BITS_H */
